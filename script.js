705724dd8311bc22569852ef30383db44b773e06
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-10-24 09:27:45 +0200

    Added DateTime

--- script.js
+++ script.js
@@ -6,52 +6,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const unmarkAllButton = document.getElementById("unmarkAll");
     const clearListButton = document.getElementById("clearList");
 
-    itemInput.addEventListener("keyup", function (event) {
-        if(event.key === "Enter") {
-            addItem();
-        }
-    });
+    // Function to update the date and time
+    function updateDateTime() {
+        const datetimeElement = document.getElementById("datetime");
+        const now = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        const formattedDateTime = now.toLocaleDateString('en-US', options);
+        datetimeElement.textContent = formattedDateTime;
+    }
+    updateDateTime();
 
-    addItemButton.addEventListener("click", function () {   
-        addItem(); 
-    });
+    setInterval(updateDateTime, 1000);
 
-    markAllButton.addEventListener("click", function () {   
-        toggleAllItems(true); 
-    });
-
-    unmarkAllButton.addEventListener("click", function () {   
-        toggleAllItems(false); 
-    });
-
-    clearListButton.addEventListener("click", function () {   
-        shoppingList.innerHTML = ''; 
-    });
-
-
+    // Function to add a new item to the list
     function addItem() {
         const itemText = itemInput.value.trim();
-        if(itemText !== "") {
-            if(isItemInList(itemText)) {
-                alert("Item is already in the list!");
-            }else {
+        if (itemText !== "") {
+            if (isItemInList(itemText)) {
+                alert("Item already in the list!");
+            } else {
                 const listItem = createListItem(itemText);
                 shoppingList.appendChild(listItem);
-                itemInput.value ="";
-            }   
+                itemInput.value = "";
+            }
         } else {
-            alert("please enter a valid item!");
+            alert("Please enter a valid item!");
         }
     }
 
+    // Function to create a new list item
     function createListItem(itemText) {
         const listItem = document.createElement("li");
-        listItem.className ="list-group-item";
+        listItem.className = "list-group-item";
         const checkbox = document.createElement("input");
-        checkbox.type ="checkbox";
+        checkbox.type = "checkbox";
 
-        checkbox.addEventListener("change", function() {
-            if(checkbox.checked) {
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
                 listItem.style.textDecoration = "line-through";
                 listItem.style.backgroundColor = "#d3e0dc";
             } else {
@@ -61,8 +52,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         const removeButton = document.createElement("button");
-        removeButton.className = "btn btn-danger";
-        removeButton.innerHTML ='<i class = "fas fa-trash-alt"></i>';
+        removeButton.className = "btn btn-danger remove-button";
+        removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
         removeButton.addEventListener("click", function () {
             shoppingList.removeChild(listItem);
         });
@@ -71,38 +62,60 @@ document.addEventListener("DOMContentLoaded", function () {
         const itemSpan = document.createElement("span");
         itemSpan.textContent = itemText;
         itemSpan.style.fontWeight = "bold";
-        listItem.appendChild(itemSpan)
-        //listItem.appendChild(document.createTextNode(itemText));
+        listItem.appendChild(itemSpan);
         listItem.appendChild(removeButton);
 
         return listItem;
     }
 
+    // Function to toggle all items as completed or not completed
     function toggleAllItems(completed) {
-        const checkboxes = shoppingList.querySelectorAll("input[type ='checkbox']");
+        const checkboxes = shoppingList.querySelectorAll("input[type='checkbox']");
         checkboxes.forEach(function (checkbox) {
             checkbox.checked = completed;
             const listItem = checkbox.parentElement;
-            if(completed) {
+            if (completed) {
                 listItem.style.textDecoration = "line-through";
                 listItem.style.backgroundColor = "#d3e0dc";
             } else {
                 listItem.style.textDecoration = "none";
                 listItem.style.backgroundColor = "transparent";
             }
-
         });
     }
 
+    // Function to check if an item is already in the list
     function isItemInList(itemText) {
         const items = shoppingList.getElementsByClassName("list-group-item");
-        for(const item of items) {
+        for (const item of items) {
             const itemSpan = item.getElementsByTagName("span")[0];
-            if(itemSpan.textContent === itemText) {
+            if (itemSpan.textContent === itemText) {
                 return true;
             }
         }
         return false;
     }
 
+    // Event listeners for the add item and buttons
+    itemInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            addItem();
+        }
+    });
+
+    addItemButton.addEventListener("click", function () {
+        addItem();
+    });
+
+    markAllButton.addEventListener("click", function () {
+        toggleAllItems(true);
+    });
+
+    unmarkAllButton.addEventListener("click", function () {
+        toggleAllItems(false);
+    });
+
+    clearListButton.addEventListener("click", function () {
+        shoppingList.innerHTML = '';
+    });
 });
